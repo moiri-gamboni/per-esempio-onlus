d20b99ade81c3731ba881e6365a7363baed44650
Moiri Gamboni <moiri.gamboni@proton.me>
2023-03-27 12:15:33 +0200

    Adds anchor links with scroll offset

--- werkstatt-child/assets/js/plugins/app.js
+++ werkstatt-child/assets/js/plugins/app.js
@@ -1,6 +1,10 @@
 // Adds inverse gradient fill hover animation for portfolio masonries
+// Adds anchor scrolling offset for top navigation menu
 (function($) {
     'use strict';
+	var win = $(window),
+		adminbar = $('#wpadminbar');
+		
     var inverseGradientFillHover = {};
     inverseGradientFillHover = {
         selector: '.type-portfolio.thb-inverse-gradient-fill-hover',
@@ -36,9 +40,44 @@
             });
         }
     };
-    
+    var hashLinks = {};
+    hashLinks = {
+      selector: '.hash-link[href*="#"]',
+      init: function() {
+        var base = this,
+          container = $(base.selector);
+        // unbind previous click events
+        container.off('click');
+        container.on('click', function(e) {
+          var _this = $(this),
+            url = _this.attr('href'),
+            ah = adminbar.outerHeight() || 0,
+            hash,
+            pos;
+          if (url) {
+            hash = url.indexOf("#") !== -1 ? url.substring(url.indexOf("#") + 1) : '';
+            pos = hash && $('#' + hash).length ? $('#' + hash).offset().top - ah - $('.header').outerHeight(): 0;
+          }
+          if (hash && pos) {
+            pos = (hash === 'footer') ? "max" : pos;
+
+            if (!$('#' + hash).hasClass('vc_tta-panel')) {
+              gsap.to(win, {
+                duration: 1,
+                scrollTo: {
+                  y: pos,
+                  autoKill: false
+                }
+              });
+            }
+            e.preventDefault();
+          }
+        });
+      }
+    };
     $(function() {
         inverseGradientFillHover.init();
+        hashLinks.init();
     });
     
 })(jQuery, this);
--- werkstatt-child/functions.php
+++ werkstatt-child/functions.php
@@ -1,10 +1,12 @@
 <?php
-function enqueue_child() {
+function enqueue_style() {
 	wp_enqueue_style( 'werkstatt-child',
 		get_stylesheet_uri(),
 		array( 'werkstatt' ),
 		wp_get_theme()->get( '1.0.0' )
 	);
+}
+function enqueue_script() {
 	wp_enqueue_script( 'werkstatt-child-app', 
 		get_stylesheet_directory_uri().'/assets/js/plugins/app.js', 
 		array('jquery')
@@ -16,5 +18,6 @@ function enqueue_templates() {
 	require_once( get_stylesheet_directory() . '/plugin/inc/vc_templates/thb_portfolio_slider.php');
 }
 
-add_action( 'wp_enqueue_scripts', 'enqueue_child' );
+add_action( 'wp_enqueue_scripts', 'enqueue_style' );
+add_action( 'after_setup_theme', 'enqueue_script' );
 add_action( 'after_setup_theme', 'enqueue_templates' );
